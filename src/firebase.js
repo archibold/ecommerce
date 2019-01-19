58acc8eab85489c1b144f82becd583cb23c31e53
Piotr Arczi <piotr.borysowski@gmail.com>
2019-01-19 22:28:23 +0100

    set properly env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,13 @@
 const config = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  databaseURL: process.env.FB_DATABASE_URL,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
+  apiKey: process.env.GATSBY_FB_API_KEY,
+  authDomain: process.env.GATSBY_FB_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FB_DATABASE_URL,
+  projectId: process.env.GATSBY_FB_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FB_MESSAGING_SENDER_ID
 }
 
 let firebaseCache
-
-export const getUiConfig = firebase => ({
-  signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-})
 
 const getFirebase = firebase => {
   if (firebaseCache) {
